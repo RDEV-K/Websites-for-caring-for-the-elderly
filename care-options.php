32fad9f3bd9ec2b7f9715140dad104b5cd0a2d6c
RDEV-K <eye.shy3294@gmail.com>
2024-01-18 21:15:38 -0800

    captcha

---
+++ care-options.php
@@ -0,0 +1,117 @@
+<?php $page_on="Care Options"?>
+<?php include 'header.php'?>
+<div class="breadcromb-wrapper">
+    <div class="breadcromb-overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-12">
+                <div class="breadcromb-left sc-content">
+                    <h3>
+                        Care Options
+                    </h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="service-details-wrapper">
+    <div class="container">
+        <div class="service-details-tabs">
+            <div class="">
+                <div id="menu1" class="tab-pane fade active in">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="col-sm-7 service-left-img sc-content">
+                                <img alt="" src="app-assets/img/9.jpg">
+                            </div>
+                            <div class="col-sm-5">
+                                <?php include 'adv-page.php'?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="tabs-content-right sc-content">
+                            <h3>
+                                St. Joseph Manor Program Statement
+                            </h3>
+                            <p>
+                                Residents are supported and encouraged to reside at the most independent level of care
+                                consistent with their needs and safety standards. Each resident’s needs are assessed by
+                                a Physician or registered nurse within 72 hours of admission. The Direct Care Manager
+                                assesses the resident periodically.
+                            </p>
+                            <p>
+                                <b>
+                                    Level I
+                                </b>
+                            </p>
+                            <p>
+                                Independent Plus. Residents at this level are functionally independent or require
+                                minimal assistance with mobility, dressing, grooming and bathing and are mentally alert.
+                                They may need nutritious meals (including a special diet), assistance with housekeeping
+                                and/or laundry and the stimulation or security of not living alone. 5 or less
+                                medications are administered by St. Joseph Staff
+                            </p>
+                            <p>
+                                <b>
+                                    Level II
+                                </b>
+                            </p>
+                            <p>
+                                Assisted Care. Resident’s needs at this level are the same as for level I, but require
+                                additional staff assistance. Medications may require special monitoring (i.e. blood
+                                work). Minimal assistance is required with one or more activities of daily living such
+                                as bathing, grooming, toileting, ambulation, transfers, eating, etc.
+                            </p>
+                            <p>
+                                <b>
+                                    Level III
+                                </b>
+                            </p>
+                            <p>
+                                Supportive Care. Residents at this level need 24-hour staff availability and may need
+                                involvement of mental health or other professionals to accommodate their needs.
+                                Residents at this level require significant daily assistance with one or more of the
+                                following.
+                            </p>
+                            <ul class="ul-c-cls">
+                                <li>
+                                    Dressing, grooming and/or bathing
+                                </li>
+                                <li>
+                                    Toileting/incontinence management
+                                </li>
+                                <li>
+                                    Transferring or mobility
+                                </li>
+                                <li>
+                                    Medication management or monitoring
+                                </li>
+                                <li>
+                                    Behavior management
+                                </li>
+                                <li>
+                                    Cueing to meals and or assistance with eating
+                                </li>
+                            </ul>
+                            <p>
+                                <b>
+                                    Level IV
+                                </b>
+                            </p>
+                            <p>
+                                Total Care. Residents at this level of care are the same as Level III with the exception
+                                that the Resident is totally dependent on St. Joseph’s Staff for their daily living
+                                needs.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'footer.php'?>
+</body>
+
+</html>
