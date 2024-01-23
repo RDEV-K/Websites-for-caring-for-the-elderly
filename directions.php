3ff4327911e54b8cb84893b9b3a963ebb3380cd7
RDEV-K <eye.shy3294@gmail.com>
2024-01-22 21:16:03 -0800

    event_detail

---
+++ directions.php
@@ -0,0 +1,47 @@
+<?php $page_on="Directions"?>
+<?php include 'header.php'?>
+<div class="breadcromb-wrapper">
+    <div class="breadcromb-overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-12">
+                <div class="breadcromb-left sc-content">
+                    <h3>
+                        Directions
+                    </h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="service-details-wrapper">
+    <div class="container">
+        <div class="service-details-tabs">
+            <div class="">
+                <div id="menu1" class="tab-pane fade active in">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="col-sm-7 service-left-img sc-content">
+                                <img alt="" src="app-assets/img/6.jpg">
+                            </div>
+                            <div class="col-sm-5">
+                                <?php include 'adv-page.php'?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="tabs-content-right sc-content">
+                            <h3>
+                                Coming Soon...
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'footer.php'?>
+</body>
+
+</html>
---
+++ employment.php
@@ -0,0 +1,100 @@
+<?php $page_on="Employment"?>
+<?php include 'header.php'?>
+<div class="breadcromb-wrapper">
+    <div class="breadcromb-overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-12">
+                <div class="breadcromb-left sc-content">
+                    <h3>
+                        Employment
+                    </h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="service-details-wrapper">
+    <div class="container">
+        <div class="service-details-tabs">
+            <div class="">
+                <div id="menu1" class="tab-pane fade active in">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="col-sm-7 service-left-img sc-content">
+                                <img alt="" src="app-assets/img/employment_1.jpg">
+                            </div>
+                            <div class="col-sm-5">
+                                <?php include 'adv-page.php'?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="tabs-content-right sc-content">
+                            <h3>
+                                St. Joseph Manor Application for Employment
+                            </h3>
+                            <p>
+                                <b>To the Applicant:</b> We appreciate your interest in our firm and assure you that we
+                                are interested in your qualifications. A clear understanding of you background and work
+                                history will aid us in seeking to place you in a position, which, in our judgment, best
+                                meets, your qualifications.
+                            </p>
+                            <p>
+                                We are an equal opportunity employer and will not unlawfully discriminate on the basis
+                                of race, color, sex, religion, national origin, age, martial or veteran status, presence
+                                of medical conditions or handicap, weight or any other protected status.
+                            </p>
+                            <p>
+                                Employers must make accommodations to disabled applicants and employees where the
+                                accommodation does not impose an undue hardship on the employer.
+                            </p>
+                            <p>
+                                Under the Michigan law only, disabled employees and applicants may request and
+                                accommodation of their disability by notifying the firm in writing of the need for
+                                accommodation within 182 days of the date the disabled individual knows of should know
+                                that an accommodation is needed. This requirement does not apply to an individual's
+                                right under the Americans with Disabilities Act. Failure to properly notify the firm may
+                                prelude any claim that the employer failed to accommodate the disabled individual.
+                            </p>
+                            <p>
+                                <b>
+                                    THIS APPLICATION WILL BE KEPT CURRENT FOR THREE MONTHS.
+                                    YOU NEED TO COMPLETE ANOTHER ONE TO BE RECONSIDERED AFTER THIS DATE.
+                                </b>
+                            </p>
+                            <p>
+                                <b>
+                                    TO APPLY FOR OUR FIRM PLEASE CLICK THE LINK BELOW AND PRINT THE APPLICATION. UPON
+                                    PRINTING IT PLEASE BRING IT IN TO OUR FACILITY ALONG WITH YOUR RESUME IN ORDER TO BE
+                                    CONSIDERED FOR EMPLOYMENT.
+                                </b>
+                            </p>
+                            <p>
+                                If you are interested in obtaining a position as a Certified Home Health Aide,
+                                Occupational Therapist, Social Worker, Physical Therapist etc. please click the link
+                                below:
+                            </p>
+                            <div class="row text-center">
+                                <div class="col-lg-offset-2 col-sm-4">
+                                    <a class="adv-a-c-cls" href="doc/application-for-employment-form.doc">
+                                        Download Application
+                                    </a>
+                                </div>
+                                <div class="col-sm-4">
+                                    <a class="adv-a-c-cls" href="mailto:info@stjosephmanor.com">
+                                        Send Completetd Application
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'footer.php'?>
+</body>
+
+</html>
