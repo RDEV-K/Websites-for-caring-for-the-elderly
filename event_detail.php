71772e1e0d4ceb7574fda236458f434ca8706921
RDEV-K <eye.shy3294@gmail.com>
2024-01-23 21:16:14 -0800

    faqs

---
+++ event_detail.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>St. Joseph Manor (Assisted Living - Affordable - Care - Comfort Compassion Excellence)</title>
+<link href="css/css.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+
+.controlstyle a{ /*links inside DIV sizecontroldiv*/
+outline:none;
+}
+
+.controlstyle a img{ /*image links inside DIV sizecontroldiv*/
+border-width:0;
+}
+
+.controlstyle a.selectedcontrol img{ /*selected control's image*/
+
+}
+
+</style>
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+
+<script type="text/javascript" src="js/fluidtextresizer.js">
+
+/***********************************************
+* Fluid Text Resizer- © Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* Visit http://www.dynamicDrive.com for hundreds of DHTML scripts
+* This notice must stay intact for legal use
+***********************************************/
+
+</script>
+
+<script type="text/javascript">
+
+var mytextsizer=new fluidtextresizer({
+	controlsdiv: "sizecontroldiv", //id of special div containing your resize controls. Enter "" if not defined
+	targets: ["body"], //target elements to resize text within: ["selector1", "selector2", etc]
+	levels: 3, //number of levels users can magnify (or shrink) text
+	persist: "session", //enter "session" or "none"
+	animate: 200 //animation duration of text resize. Enter 0 to disable
+})
+
+function emailCheck (emailStr) {
+	var sw = true;
+	var checkTLD=0;
+	var knownDomsPat=/^(com|net|org|edu|int|mil|gov|arpa|biz|aero|name|coop|info|pro|museum)$/;
+	var emailPat=/^(.+)@(.+)$/;
+	var specialChars="\\(\\)><@,;:\\\\\\\"\\.\\[\\]";
+	var validChars="\[^\\s" + specialChars + "\]";
+	var quotedUser="(\"[^\"]*\")";
+	var ipDomainPat=/^\[(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})\]$/;
+	var atom=validChars + '+';
+	var word="(" + atom + "|" + quotedUser + ")";
+	var userPat=new RegExp("^" + word + "(\\." + word + ")*$");
+	var domainPat=new RegExp("^" + atom + "(\\." + atom +")*$");
+	var matchArray=emailStr.match(emailPat);
+	if (matchArray==null) { sw = false; return sw; }
+	var user=matchArray[1];
+	var domain=matchArray[2];
+	for (i=0; i<user.length; i++) {
+		if (user.charCodeAt(i)>127) sw = false;				
+	}
+	for (i=0; i<domain.length; i++) {
+		if (domain.charCodeAt(i)>127) sw = false;
+	}
+	if (user.match(userPat)==null) sw = false;
+	var IPArray=domain.match(ipDomainPat);
+	if (IPArray!=null) {
+		for (var i=1;i<=4;i++) {
+			if (IPArray[i]>255) sw = false;
+		}
+		return true;
+	}
+	var atomPat=new RegExp("^" + atom + "$");
+	var domArr=domain.split(".");
+	var len=domArr.length;
+	for (i=0;i<len;i++) {
+		if (domArr[i].search(atomPat)==-1) sw = false;
+	}
+	if (checkTLD && domArr[domArr.length-1].length!=2 && domArr[domArr.length-1].search(knownDomsPat)==-1) sw = false;
+	if (len<2) sw = false;
+	return sw;
+}
+function frm_submit(){
+	if(document.getElementById('textfield').value==""){
+		alert('Please enter the first name.');
+		document.getElementById('textfield').focus();
+		return false;
+	}else if(document.getElementById('textfield2').value==""){
+		alert('Please enter the last name.');
+		document.getElementById('textfield2').focus();
+		return false;
+	}else if(document.getElementById('textfield3').value==""){
+		alert('Please enter the email address.');
+		document.getElementById('textfield3').focus();
+		return false;
+	}else if(!emailCheck(document.getElementById('textfield3').value)){
+		alert('Please enter the correct email address.');
+		document.getElementById('textfield3').focus();
+		return false;
+	}else if(document.getElementById('textfield4').value==""){
+		alert('Please enter the phone number.');
+		document.getElementById('textfield4').focus();
+		return false;
+	}else if(document.getElementById('textfield5').value==""){
+		alert('Please enter the message.');
+		document.getElementById('textfield5').focus();
+		return false;
+	}else{
+		return true;
+	}
+}
+</script>
+</head>
+
+<body>
+<?php
+include_once "db.php";
+?>
+<table width="100%" cellspacing="0" cellpadding="0">
+  <tr>
+    <td align="center" valign="top" class="topinner"><table width="960" align="center" cellpadding="0" cellspacing="0">
+      <tr>
+        <td class="header-logo"><a href="index.php" title="St. Joseph Manor (Assisted Living - Affordable Care - Comfort Compassion Excellence)"><img src="images/st-joseph-manor-assisted-li.jpg" width="525" height="107" border="0" /></a></td>
+        <td class="header-phone"><table width="100%" cellspacing="0" cellpadding="0">
+            <tr>
+              <td class="fontsize"><div id="sizecontroldiv" class="controlstyle">FONT SIZE <span class="bracket">{</span><a href="#fontsize0" class="size2">A</a> <a href="#fontsize1" class="size3">A</a><span class="bracket">}</span> </div></td>
+            </tr>
+            <tr>
+              <td class="fontsize">Phone: (313)-882-3800 | <a href="faqs.php" class="fontsize" title="FAQs">FAQs</a> | <a href="location-map.php" class="fontsize" title="Location">LOCATION</a></td>
+            </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td colspan="2" class="jquery1"><table width="960" cellspacing="0" cellpadding="0" height="350px;">
+            <tr>
+              <td align="left" valign="top" class="nav1"><?php include("navigation.php"); ?></td>
+              <td valign="bottom" class="tabs"><table width="97%" align="left" cellpadding="0" cellspacing="0">
+                  <tr>
+                    <td width="25%" align="center"><a href="request-info.php" title="Request Info"><img src="images/request-info.jpg" width="93" height="93" border="0" /></a></td>
+                    <td width="25%" align="center"><a href="refer-a-friend.php" title="Refer A Friend"><img src="images/refer-a-friend.jpg" width="93" height="93" border="0" /></a></td>
+                    <td width="25%" align="center"><a href="respite-care.php" title="Respite Care"><img src="images/respite-care.jpg" width="93" height="93" border="0" /></a></td>
+                    <td width="25%" align="center"><a href="suites-tour.php" title="Suites Tour"><img src="images/suites-tour.jpg" width="93" height="93" border="0" /></a></td>
+                  </tr>
+              </table></td>
+            </tr>
+        </table></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="center" valign="top"><div align="center" style="padding-top:20px;">
+      <table width="960" align="center" cellpadding="0" cellspacing="0">
+        <tr>
+          <td width="527" align="left" valign="top"><table width="95%" cellspacing="0" cellpadding="0">
+              <tr>
+                <td height="55" align="left" class="title">Events</td>
+              </tr>
+              <tr>
+                <td align="left"><div align="justify" style="padding-top:2px; line-height:22px;">
+                    <?php
+							if($_REQUEST['id']>0){
+								$id=$_REQUEST['id'];
+								$whr=" and id='$id'";
+							}
+							$sql_get="select * from events where active='1' $whr order by id desc limit 0,4";
+							$sql_qry=mysql_query($sql_get);
+							$chk_whr_num=mysql_num_rows($sql_qry);
+							while($fet_row=mysql_fetch_array($sql_qry)){
+							?>
+                    <table width="100%" cellspacing="0" cellpadding="0">
+                      <tr>
+                        <td width="4%" align="left"><img src="images/calendar.png" width="21" height="23" /></td>
+                        <td width="88%" align="left"><strong><a href="event_detail.php?id=<?php echo $fet_row['id']; ?>" class="bottomtextsmall"><?php echo $fet_row['title']; ?></a></strong></td>
+                      </tr>
+                      <tr>
+                        <td colspan="2" align="left"><?php 
+									$dat_exp=explode('-',$fet_row['event_date']);
+									echo date('l, d M Y',mktime(0,0,0,$dat_exp[1],$dat_exp[2],$dat_exp[0])); 
+								?>
+                          at
+                          <?php 
+									$st_exp=explode(':',$fet_row['start_time']);
+									 echo date('h:i A',mktime($st_exp[0],$st_exp[1],$st_exp[2],$dat_exp[1],$dat_exp[2],$dat_exp[0])); 
+								?>
+                          -
+                          <?php 
+									$end_exp=explode(':',$fet_row['end_time']);
+									echo date('h:i A',mktime($end_exp[0],$end_exp[1],$end_exp[2],$dat_exp[1],$dat_exp[2],$dat_exp[0])); 
+								?>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td colspan="2" align="left"><?php echo $fet_row['description']; ?> </td>
+                      </tr>
+                    </table>
+                  <?php
+							}
+							?>
+                </div></td>
+              </tr>
+          </table></td>
+          <td width="433" align="left" valign="top"><?php include("event-right.php"); ?></td>
+        </tr>
+        <tr>
+          <td colspan="2">&nbsp;</td>
+        </tr>
+      </table>
+    </div></td>
+  </tr>
+  <tr>
+    <td align="center" valign="bottom" class="bottom"><?php include("bottom.php"); ?></td>
+  </tr>
+</table>
+
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,109 @@
+<footer class="footer-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2 col-sm-4">
+                <div class="intro">
+                    <div class="flogo">
+                        <a href="/">
+                            <div class="sc-content">
+                                <img class="img-responsive" src="app-assets/img/title-image.png" alt="">
+                                <!-- <h2 class="company-name">A-1 Jc Painting and Drywall Inc</h2> -->
+                                <!-- <h4 class="slogan-name">Coloring Your Dream.</h4> -->
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-4">
+                <div class="sc-content">
+                    <h4 class="title m-t-130-c-cls">Our Services</h4>
+                    <ul class="main_services_ul">
+                        <li><a href="services.php">Services</a></li>
+                        <li><a href="amenities.php">Amenities</a></li>
+                        <li><a href="suites-tour.php">Suites Tour</a></li>
+                        <li><a href="care-options.php">Care Options</a></li>
+                        <li><a href="from-our-administrator.php">From Our Administrator</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-2 col-sm-4">
+                <div class="sc-content">
+                    <h4 class="title">Useful Links</h4>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About Us</a></li>
+                        <li> <a href="testimonials.php">Testimonials</a></li>
+                        <li><a href="employment.php">Employment</a></li>
+                        <li><a href="contact-us.php">Contact Us</a></li>
+
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-2 col-sm-4">
+                <div class="sc-content">
+                    <h4 class="title">Other Services</h4>
+                    <ul>
+                        <li><a href="submit-testimonials.php">Submit Testimonial</a></li>
+                        <li><a href="refer-a-friend.php">Refer A Friend</a></li>
+                        <li> <a href="request-info.php">Request Info</a></li>
+                        <li><a href="respite-care.php"> Respite Care</a></li>
+                        <li> <a href="directions.php">Directions</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-4">
+                <div class="address sc-content">
+                    <h4 class="title">Contact Us</h4>
+                    <ul>
+
+                        <li>
+                            <span class="fcontact-icon fa fa-map-marker"></span>
+                            Near Interstate 94
+                            <br>
+                            4800 Cadieux, Detroit, MI 48224
+                        </li>
+                        <li><span class="fcontact-icon fa fa-phone">
+                            </span>
+                            <a href="tel: (313)-882-3800">
+                                (313)-882-3800</a>
+                        </li>
+                        <li><span class="fcontact-icon fa fa-fax"></span> <a
+                                href="tel:(313)-882-5940">(313)-882-5940</a>
+                        </li>
+                        <li><span class="fcontact-icon fa fa-envelope"></span> <a
+                                href="mailto:info@stjosephmanor.com">info@stjosephmanor.com</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-cta-wrapper">
+            <div class="row">
+                <div class="col-sm-9 sc-content">
+                    <img src="app-assets/img/24-hours-support.png" alt="">
+                    <h3>St. Joseph Manor is a community of loving people</h3>
+                    <p>24 Hours Emergency Services Available</p>
+                </div>
+                <div class="col-sm-3 sc-content">
+                    <a href="privacy-policy.php" class="pull-right">
+                        Privacy Policy
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- footer -->
+<script src="app-assets/js/jquery-3.1.1.min.js"></script>
+<script src="app-assets/js/bootstrap.min.js"></script>
+<script src="app-assets/js/slicknav.min.js"></script>
+<script src="app-assets/js/lightbox.min.js"></script>
+<script>
+$(function() {
+    $('.menu').slicknav({
+        prependTo: ".mobile-menu",
+        duration: 600,
+        closeOnClick: true,
+    });
+})
+</script>
