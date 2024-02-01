b3d56b18685af41af27f127768f357274b5ba527
RDEV-K <eye.shy3294@gmail.com>
2024-01-31 21:17:33 -0800

    services

---
+++ services.php
@@ -0,0 +1,80 @@
+<?php $page_on="Service"?>
+<?php include 'header.php'?>
+<div class="breadcromb-wrapper">
+    <div class="breadcromb-overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-12">
+                <div class="breadcromb-left sc-content">
+                    <h3>
+                        Services
+                    </h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="service-details-wrapper">
+    <div class="container">
+        <div class="service-details-tabs">
+            <div class="">
+                <div id="menu1" class="tab-pane fade active in">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="col-sm-7 service-left-img sc-content">
+                                <img alt="" src="images/suites/big/suite-tour4.jpg">
+                            </div>
+                            <div class="col-sm-5">
+                                <?php include 'adv-page.php'?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="tabs-content-right sc-content">
+                            <h3>
+                                Personalized Services
+                            </h3>
+                            <ul class="ul-c-cls">
+                                <li>
+                                    Beauty Salon/Barber Shop/Manicures
+                                </li>
+                                <li>
+                                    24 Hour Front Desk Receptionist/Security
+                                </li>
+                                <li>
+                                    Secured Entrances
+                                </li>
+                                <li>
+                                    St. Joseph Manor's priority is always the safety of our residents and staff
+                                </li>
+                                <li>
+                                    Utilities/Maintenance
+                                </li>
+                                <li>
+                                    All utilities are included except cable and telephone.
+                                </li>
+                                <li>
+                                    Laundry and Housekeeping Services
+                                </li>
+                                <li>
+                                    On site Home Health Care
+                                </li>
+
+
+                            </ul>
+                            <p>
+                                Allied Home Health Care and Nursing Services, LLC is our on site Home Health Care Agency
+                                that offers Nursing Services, Physical Therapy, Occupational Therapy, Speech Therapy,
+                                Medical Social Services, Home Health Aides and Medical Supplies
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'footer.php'?>
+</body>
+
+</html>
